d9a09f38e1a18a30a50e839bf44dfd2d7c36f59f
alejandroleon98 <alexleon8898@gmail.com>
2020-11-22 16:49:39 +0100

    limpiar

--- src/main/java/es/uniovi/ssii/rb/UO258774.java
+++ src/main/java/es/uniovi/ssii/rb/UO258774.java
@@ -3,11 +3,7 @@ package es.uniovi.ssii.rb;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-
 import org.openmarkov.core.exception.IncompatibleEvidenceException;
-import org.openmarkov.core.exception.InvalidStateException;
-import org.openmarkov.core.exception.NodeNotFoundException;
 import org.openmarkov.core.exception.NotEvaluableNetworkException;
 import org.openmarkov.core.model.network.EvidenceCase;
 import org.openmarkov.core.model.network.Finding;
@@ -22,13 +18,37 @@ import org.openmarkov.inference.likelihoodWeighting.LogicSampling;
 public class UO258774 {
 
 	private static ProbNet probNet;
-	private Long seed;
-	private static Random rnd;
+
+	public static void main(String[] args) throws Exception {
+
+		UO258774 obj = new UO258774("asia.pgmx");
+
+		System.out.format("Network \"%s\" with %d nodes and %d links\n", obj.getProbNet().getName(),
+				obj.getProbNet().getNumNodes(), obj.getProbNet().getLinks().size());
+
+		//Evidencia
+		EvidenceCase evidence = new EvidenceCase();
+		Variable variable = probNet.getVariables().get(5);
+		Variable variable2 = probNet.getVariables().get(0);
+		evidence.addFinding(probNet, variable.getName(), "no");
+		evidence.addFinding(probNet, variable2.getName(), "no");
+
+		//Variables de interés
+		List<Variable> variablesOfInterest = new ArrayList<Variable>();
+		List<Variable> possibleVariables = probNet.getVariables();
+		variablesOfInterest.add(possibleVariables.get(7));
+
+
+		System.out.println("######## Inferencia aproximada ########");
+		System.out.println("#### Muestreo estocastico (LogicSampling) ####");
+
+		obj.LSInference(variablesOfInterest, evidence);
+
+
+	}
 
 	public UO258774(String fileName) throws Exception {
 		probNet = NetsIO.openNetworkFile("src/main/resources/networks/"+fileName).getProbNet();
-		seed = null;
-		rnd = new Random();
 	}
 
 	public ProbNet getProbNet() {
@@ -38,15 +58,6 @@ public class UO258774 {
 	@SuppressWarnings("static-access")
 	public void setProbNet(ProbNet probNet) {
 		this.probNet = probNet;
-	}
-
-	public Long getSeed() {
-		return seed;
-	}
-
-	public void setSeed(Long seed) {
-		this.seed = seed;
-		rnd.setSeed(seed);
 	}
 
 	public long LSInference(List<Variable> variablesOfInterest, EvidenceCase evidence) {
@@ -97,82 +108,6 @@ public class UO258774 {
 
 	public static void printTime(long nanoseconds) {
 		System.out.format("Total time: %.2f miliseconds\n", nanoseconds / 1000000.0);
-	}
-
-	/**
-	 * Returns a evidence case picking randomly a set of variables and a random
-	 * state for each one of them
-	 * 
-	 * @param numEvidenceVariables the number of variables in the evidence
-	 * @return a evidence case
-	 */
-	public EvidenceCase getRandomEvidence(int numEvidenceVariables) {
-		EvidenceCase evidence = new EvidenceCase();
-
-		try {
-			List<Variable> variablesToPick = probNet.getVariables();
-			for (int i = 0; i < numEvidenceVariables && variablesToPick.size() > 0; i++) {
-				int idx = rnd.nextInt(variablesToPick.size());
-				Variable variable = variablesToPick.get(idx);
-				variablesToPick.remove(idx);
-				String name = variable.getName();
-				String state = variable.getStates()[rnd.nextInt(variable.getNumStates())].getName();
-
-				evidence.addFinding(probNet, name, state);
-			}
-		} catch (NodeNotFoundException | InvalidStateException | IncompatibleEvidenceException e) {
-			e.printStackTrace();
-		}
-
-		return evidence;
-	}
-
-	/**
-	 * Returns a list of variables of interest, not included in the evidence for the
-	 * current network
-	 * 
-	 * @param numVoI   number of variables to select
-	 * @param evidence the evidence
-	 * @return the list of variables selected
-	 */
-	public List<Variable> getRandomVariablesOfInterest(int numVoI, EvidenceCase evidence) {
-		List<Variable> variablesOfInterest = new ArrayList<Variable>();
-		List<Variable> possibleVariables = probNet.getVariables();
-		possibleVariables.removeAll(evidence.getVariables());
-		for (int i = 0; i < numVoI && i < possibleVariables.size(); i++) {
-			int index = rnd.nextInt(possibleVariables.size());
-			variablesOfInterest.add(possibleVariables.get(index));
-			possibleVariables.remove(index);
-		}
-		return variablesOfInterest;
-	}
-
-	public static void main(String[] args) throws Exception {
-
-		UO258774 obj = new UO258774("asia.pgmx");
-
-		System.out.format("Network \"%s\" with %d nodes and %d links\n", obj.getProbNet().getName(),
-				obj.getProbNet().getNumNodes(), obj.getProbNet().getLinks().size());
-
-		//Evidencia
-		EvidenceCase evidence = new EvidenceCase();
-		Variable variable = probNet.getVariables().get(5);
-		Variable variable2 = probNet.getVariables().get(0);
-		evidence.addFinding(probNet, variable.getName(), "no");
-		evidence.addFinding(probNet, variable2.getName(), "no");
-
-		//Variables de interés
-		List<Variable> variablesOfInterest = new ArrayList<Variable>();
-		List<Variable> possibleVariables = probNet.getVariables();
-		variablesOfInterest.add(possibleVariables.get(7));
-
-
-		System.out.println("######## Inferencia aproximada ########\n");
-		System.out.println("#### Muestreo estocastico (LogicSampling) ####");
-
-		obj.LSInference(variablesOfInterest, evidence);
-
-
 	}
 
 }
